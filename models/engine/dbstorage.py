8d48a13a12a167fefa6dec2476c093c9058f62fb
frimpongrijkaard <frimpongrijkaard4@yahoo.com>
2024-09-10 14:02:33 -0400

    setup mysql to connect the database in main project

--- models/engine/dbstorage.py
+++ models/engine/dbstorage.py
@@ -3,15 +3,18 @@ import json
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from os import getenv
-from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy.ext.declarative import declarative_base
 from models.user import User
 from models.customer import Customer
 from models.payment import Payment
 from models.utility import Utility
+from models.customutility import CustomerUtility
 """
 module store file of any instance created write and read them
-
 """
+#Base = declarative_base()
+
 class DBStorage:
     """
         Private class attribute:
@@ -19,18 +22,18 @@ class DBStorage:
         __session: store object
    
     """
-    __file_path = None
+    __engine = None
     __session = None
 
     def __init__(self):
 
-        pay_dev = getenv('PAY_MYSQL_USE')
-        pay_dev_pwd = getenv('PAY_MYSQL_PWD')
+        user = getenv('PAY_MYSQL_USE')
+        pwd = getenv('PAY_MYSQL_PWD')
         host = getenv('PAY_MYSQL_HOST')
-        pay_dev_db = getenv('PAY_MYSQL_DB')
-        db_url = "mysql//{}:{}@{}:3306/{}".format(pay_dev, pay_dev_pwd, host, pay_dev_db)
+        db = getenv('PAY_MYSQL_DB')
+        db_url = 'mysql+mysqldb://{}:{}@{}/{}?charset=utf8mb4'.format(user, pwd, host, db)
 
-        self.__engine = create_engine(bind=db=url, pool_pre_ping=True)
+        self.__engine = create_engine(db_url, pool_pre_ping=True, echo=True)
 
     def classes(self):
         """ modules class for utility payment system"""
@@ -40,27 +43,28 @@ class DBStorage:
                    "Customer": Customer,
                    "Session": Session,
                    "Payment": Payment,
+                   "CustomerUtility":CustomerUtility}
         return classes
     
     def all(self, cls=None):
         """Method that retrives all object created from system"""
-        if cls not None:
+        if cls:
             objs = self.__session.query(self.classes()[cls]).all()
         else:
-            objs = self.__session.query(User).all()
-            objs = self.__session.query(Customer).all()
-            objs = self.__session.query(Payment).all()
-            objs = self.__session.query(Utility).all()
+            objs += self.__session.query(User).all()
+            objs += self.__session.query(Customer).all()
+            objs += self.__session.query(Payment).all()
+            objs += self.__session.query(Utility).all()
 
         ob_dic = {}
         for obj in objs:
-            key = "{}.{}".format(self.classes()[cls], obj.id
+            key = "{}.{}".format(self.classes()[cls], obj.id)
             ob_dic[cls] = obj
         return ob_dic
     
     def new(self, obj):
        """Method created new object and it been store in DBstorage"""
-        self.__session.add(obj)
+       self.__session.add(obj)
 
     def save(self):
         self.__session.commit()
@@ -70,10 +74,17 @@ class DBStorage:
         from models.user import User
         from models.utility import Utility
         from models.payment import Payment
-        from modles.customer import Customer
+        from models.customer import Customer
+        from models.customutility import CustomerUtility
 
         Base.metadata.create_all(self.__engine)
-        self.__session = sessionmaker(bind=self.__session, expire_on_commit=False)
-        Session = scoped_session(self.__session)
+
+        sess = sessionmaker(bind=self.__session, expire_on_commit=False)
+
+        Session = scoped_session(sess)
+
         self.__session = Session()
+
+    def close(self):
+        return self.__session.close()
 
